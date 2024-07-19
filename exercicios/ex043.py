d5e2a976d6b7734616fd0ae602d05e2164cc8050
Marcelo Melo <melo.marcelodev@gmail.com>
2024-07-19 10:06:34 -0300

    Update ex043.py

--- exercicios/ex043.py
+++ exercicios/ex043.py
@@ -1,22 +1,23 @@
 import math
 peso = float(input('Informe o peso da pessoa: (Kg)'))
 altura = float(input('Informe o altura da pessoa: (m)'))
-imc = peso / altura**2
+imc = peso / (altura**2)
 if imc < 18.5:
     print(f'O IMC dessa pessoa é {imc:.2f}:')
     print('Você está abaixo do peso.')
-elif imc == 18.5 < 25:
+elif imc >= 18.5 and imc <= 25:
         print(f'Você está com o IMC de {imc:.2f}:')
         print('Seu peso está normal!')
-elif imc == 25 < 30:
+elif imc >=25 and imc <= 30:
       print(f'Você está com o IMC de {imc:.2f}:')
       print('Você esta com sobrepeso!')
-elif imc > 30 < 40:
+elif imc >= 30 and imc <= 40:
       print(f'Você está com o IMC de {imc:.2f}.')
       print('Você esta com Obesidade!')
-elif imc >= 40:
-      print(f'Você esta com o ICM {imc:.2f}:')
-      print('Você está com Obesidade Morbida!')
+else:
+     print(f'Você está com o ICM de {imc:.2f}')
+     print('Você está com obesidade morbida!')
+
 
     
 
