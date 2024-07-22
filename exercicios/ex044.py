ad0297967ed6de7a6cbfd323d393a21ed780414f
Marcelo Melo <melo.marcelodev@gmail.com>
2024-07-22 05:48:05 -0300

    Update ex044.py

--- exercicios/ex044.py
+++ exercicios/ex044.py
@@ -6,6 +6,8 @@ print('[1] á vista Dinheiro ou cheque:')
 print('[2] á vista cartão de crédito:')
 print('[3] 2x no cartão de crédito:')
 print('[4] 3x ou mais no cartão de crédito:')
-opção = input('Qual é a sua opção?')
-if preço == 1:
-    print('')
+opção = int(input('Qual a sua opção?'))
+if opção ==1:
+    print(f'Sua compra de {preço} vai custar {(preço*10/100)-(preço)}')
+elif opção == 2:
+    print(f'Sua compra de {preço:.1f} vai custar {preço*5//100 - preço}')
