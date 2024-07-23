e653d879669c080a8b5c4260b0803db39b0fea08
Marcelo Melo <melo.marcelodev@gmail.com>
2024-07-23 06:00:50 -0300

    Create ex045.py

---
+++ exercicios/ex045.py
@@ -0,0 +1,19 @@
+from random import randint
+from time import sleep
+print('Suas opções:')
+print('[0] Pedra')
+print('[1] Papel')
+print('[2] Tesoura')
+pedra = 0
+papel = 1
+tesoura = 2
+jogada = (input('Qual a sua jogada?'))
+sleep(1)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ')
+
+
+
