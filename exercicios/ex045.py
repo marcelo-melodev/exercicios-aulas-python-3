db63f9d063d0738197900e95969eeed94bbf7e20
Marcelo Melo <melo.marcelodev@gmail.com>
2024-07-25 09:05:42 -0300

    Update ex045.py

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -4,33 +4,19 @@ print('Suas opções:')
 print('[0] Pedra')
 print('[1] Papel')
 print('[2] Tesoura')
-pedra = 0
-papel = 1
-tesoura = 2
+pedra = 'Pedra'
+papel = 'Papel'
+tesoura = 'Tesoura'
 jogada = (input('Qual a sua jogada?'))
-p0 = pedra
-p1 = papel
-t2 = tesoura
 sleep(1)
 print('JO')
 sleep(1)
 print('KEN')
 sleep(1)
 print('PÔ')
-sorteio = int(randint(0,2))
-if sorteio == 0:
-    print(f'''====================
-          Computador jogou Pedra
-          ==============================''')
-elif sorteio == 1:
-    print('''===================
-          Computador jogou Papel
-          ===============================''')
-elif sorteio >= 2:
-    print('''======================
-          Computador jogou Tesoura
-          ===============================''')
-
-
-
-
+sorteio = (randint(0,2))
+if sorteio <= 0  or sorteio == 1 or sorteio >= 2:
+    print(f'''==========================
+    Computador jogou {sorteio}
+    Jogador Jogou {jogada}
+============================''')
