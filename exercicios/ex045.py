f25acef5af400c9b817cce7894f83868e19570eb
Marcelo Melo <melo.marcelodev@gmail.com>
2024-07-26 06:42:01 -0300

    Update ex045.py

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -4,10 +4,8 @@ print('Suas opções:')
 print('[0] Pedra')
 print('[1] Papel')
 print('[2] Tesoura')
-pedra = 'Pedra'
-papel = 'Papel'
-tesoura = 'Tesoura'
-jogada = (input('Qual a sua jogada?'))
+itens = ('Pedra', 'Papel', 'Tesoura')
+jogada = int(input('Qual a sua jogada?'))
 sleep(1)
 print('JO')
 sleep(1)
@@ -15,8 +13,39 @@ print('KEN')
 sleep(1)
 print('PÔ')
 sorteio = (randint(0,2))
-if sorteio <= 0  or sorteio == 1 or sorteio >= 2:
-    print(f'''==========================
-    Computador jogou {sorteio}
-    Jogador Jogou {jogada}
+print(f'''==========================
+    Computador jogou {itens[sorteio]}
+    Jogador Jogou {itens[jogada]}
 ============================''')
+if sorteio ==0:
+    if jogada ==0:
+        print('EMPATE')
+    elif jogada ==1:
+        print('Jogador Vence')
+    elif jogada ==2:
+        print('Omputador vence')
+    else:
+        print('Jogada Invalida!')
+elif sorteio ==1:
+    if jogada ==0:
+        print('Computador Vence')
+    elif jogada ==1:
+        print('Empate')
+    elif jogada ==2:
+        print('Jogador Vence')
+    else:
+        print('Jogada Inválida')
+elif sorteio ==2:
+    if jogada == 0:
+        print('jogador Vence')
+    elif jogada ==1:
+        print('Cmputador Vence')
+    elif jogada ==2:
+        print('Empate')
+    else:
+        print('Jogada Inválida')
+
+
+
+
+
