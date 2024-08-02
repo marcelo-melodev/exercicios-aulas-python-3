5d4872b00431d011500cd7f7a119c6af40dfd776
Marcelo Melo <melo.marcelodev@gmail.com>
2024-08-02 06:23:46 -0300

    Update ex046.py

--- exercicios/ex046.py
+++ exercicios/ex046.py
@@ -1,6 +1,6 @@
 import emoji
 from time import sleep
-print('Iniciando a contagem para o ano novo 🎉')
+print('\033[1;31;40mIniciando a contagem para o ano novo 🎉\033m')
 for c in range(10, 0, -1):
     sleep(1)
     print(c)
