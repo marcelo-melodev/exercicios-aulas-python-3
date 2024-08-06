906448624faf2d8dfadc5a37f40bd734afdfa7fe
Marcelo Melo <melo.marcelodev@gmail.com>
2024-08-06 08:39:47 -0300

    Update ex050.py

--- exercicios/ex050.py
+++ exercicios/ex050.py
@@ -1,13 +1,14 @@
-n1 = int(input('Digite um numero:'))
-n2 = int(input('Digite um numero:'))
-n3 = int(input('Digite um numero:'))
-n4 = int(input('Digite um numero:'))
-n5 = int(input('Digite um numero:'))
-n6 = int(input('Digite um numero:'))
-somapar = n1+n2+n3+n4+n5+n6
-somaimpar = n1+n2+n3+n4+n5+n6
-if somapar % 2 == 0:
-        print(somapar)
-elif somaimpar %2 == 1:
-        print()
+s = 0
+for c in range(0, 6):
+    num = int(input('Digite um numero:'))
+    s += num
+if num %2 == 0:
+    print(f'{s}')
+else:
+    print()
+
+
+
+
+
         
